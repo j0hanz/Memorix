7f48291e2e430996d9435d8f2c784c44d12d1fac
Linus Johansson <golfarenlj@gmail.com>
2025-01-30 14:05:30 +0000

    Refactor card styles and update game title; replace symbol with card back image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import Game from "@/components/Game";
 import LoadingSpinner from "@/components/Spinner";
 import { GameInstructions, LatestUpdates } from "@/components/Modal";
@@ -10,6 +10,7 @@ import { HiNewspaper } from "react-icons/hi2";
 import { LiaGithub } from "react-icons/lia";
 import { useGameHandlers } from "@/utils/gameHandlers";
 import { motion, Variants } from "framer-motion";
+import cardBack from "./assets/img/card-back.webp";
 import styles from "@/App.module.css";
 
 const App: React.FC = () => {
@@ -66,7 +67,7 @@ const App: React.FC = () => {
           transition={pageTransition}
         >
           <div className={styles.menu}>
-            <span className={styles.symbol}>&</span>
+            <Image src={cardBack} className={styles.cardBack} fluid />
             <div className={styles.gameTitle}>GIFMatch</div>
             <StartButton onClick={startGame} />
             <InstructionsButton onClick={openInstructions} />
