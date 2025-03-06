5a251b9b0c73866e07f61f9cdf463b46af909a32
j0hanz <golfarenlj@gmail.com>
2025-03-06 14:01:11 +0100

    Refactor configuration files and update dependencies for improved linting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: '.',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
+      react,
+    },
+    settings: {
+      react: {
+        version: '19.0',
+        fragment: 'Fragment',
+      },
+      linkComponents: [{ name: 'Link', linkAttribute: 'to' }],
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react-compiler/react-compiler': 'error',
+      'react/jsx-curly-brace-presence': [
+        'error',
+        { props: 'never', children: 'never' },
+      ],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'react/no-unstable-nested-components': 'warn',
+      'react/prop-types': 'off',
+    },
+  },
+);
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
       '@': '/src',
