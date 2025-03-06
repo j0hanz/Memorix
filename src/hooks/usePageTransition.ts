94167b29b115a3fddb1e7edeb06fbae6a12cede6
j0hanz <golfarenlj@gmail.com>
2025-03-06 19:17:40 +0100

    Add usePageTransition hook for managing page transition animations

---
+++ src/hooks/usePageTransition.ts
@@ -0,0 +1,26 @@
+import { Variants } from 'framer-motion';
+
+export function usePageTransition() {
+  const pageVariants: Variants = {
+    initial: {
+      opacity: 0,
+      scale: 0.7,
+    },
+    in: {
+      opacity: 1,
+      scale: 1,
+    },
+    out: {
+      opacity: 0,
+      scale: 0.7,
+    },
+  };
+
+  const pageTransition = {
+    type: 'spring',
+    stiffness: 50,
+    damping: 20,
+  };
+
+  return { pageVariants, pageTransition };
+}
