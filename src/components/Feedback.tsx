2b27bfcab0c515ec2f817b76ba751978d452cd4c
j0hanz <golfarenlj@gmail.com>
2025-03-09 10:14:53 +0100

    Add animation to feedback messages using framer-motion

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,5 +1,7 @@
 import { HiCheck, HiXMark } from 'react-icons/hi2';
 import styles from './styles/Feedback.module.css';
+import { motion, AnimatePresence } from 'framer-motion';
+import { usePageTransition } from '@/hooks/usePageTransition';
 import { FEEDBACK } from '@/utils/constants';
 
 interface FeedbackProps {
@@ -8,15 +10,27 @@ interface FeedbackProps {
 
 // Display feedback messages based on the message prop
 const Feedback = ({ message }: FeedbackProps) => {
+  // Get animation settings for page transitions
+  const { feedbackVariants } = usePageTransition();
   if (!message) return null;
 
   return (
     <>
-      {message === FEEDBACK.SUCCESS ? (
-        <HiCheck className={styles.success} />
-      ) : (
-        <HiXMark className={styles.error} />
-      )}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={message}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={feedbackVariants}
+        >
+          {message === FEEDBACK.SUCCESS ? (
+            <HiCheck className={styles.success} />
+          ) : (
+            <HiXMark className={styles.error} />
+          )}
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 };
