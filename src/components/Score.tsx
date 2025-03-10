6f2c10b0adcda2cfaeaac2d9b8c2252412ccafa8
j0hanz <golfarenlj@gmail.com>
2025-03-10 16:05:21 +0100

    Refactor Score component to use useGameScore hook for star rating calculation

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,6 +1,6 @@
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
 import styles from './styles/GameStats.module.css';
-import { SCORING } from '@/utils/constants';
+import { useGameScore } from '@/hooks/useGameScore';
 
 interface ScoreProps {
   moves: number;
@@ -8,20 +8,7 @@ interface ScoreProps {
 }
 
 export default function Score({ moves, completedTime }: ScoreProps) {
-  // Helper function to compute the star rating using constants
-  function getStars(moves: number, time: number): number {
-    if (moves <= SCORING.FIVE_STAR.moves && time <= SCORING.FIVE_STAR.time)
-      return 5;
-    if (moves <= SCORING.FOUR_STAR.moves && time <= SCORING.FOUR_STAR.time)
-      return 4;
-    if (moves <= SCORING.THREE_STAR.moves && time <= SCORING.THREE_STAR.time)
-      return 3;
-    if (moves <= SCORING.TWO_STAR.moves && time <= SCORING.TWO_STAR.time)
-      return 2;
-    return 1;
-  }
-
-  const stars = getStars(moves, parseInt(completedTime, 10));
+  const { stars } = useGameScore(moves, parseInt(completedTime, 10));
 
   return (
     <>
