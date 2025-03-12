a5dd9ddcdb19ff9af2abbdf30256c8d78f9443a1
j0hanz <golfarenlj@gmail.com>
2025-03-12 20:54:30 +0100

    Refactor shuffleCards function into utils for better code organization

--- src/hooks/useShuffledDeck.ts
+++ src/hooks/useShuffledDeck.ts
@@ -1,21 +1,6 @@
 import { useState } from 'react';
 import { PairedCard, generateCards } from '@/data/cardData';
-
-// Custom hook to shuffle a list of cards
-function shuffleCards<T>(cards: T[]): T[] {
-  if (!Array.isArray(cards)) {
-    throw new TypeError('Input must be an array');
-  }
-  if (cards.length === 0) return [];
-
-  const shuffled = [...cards];
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    // Swap elements
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
-}
+import { shuffleCards } from '@/utils/deckUtils';
 
 export function useShuffledDeck() {
   // Generate and shuffle deck initially
@@ -46,8 +31,5 @@ export function useShuffledDeck() {
     deck,
     setDeck,
     refreshDeck,
-    shuffleCards,
   };
 }
-
-export { shuffleCards };
---
+++ src/utils/deckUtils.ts
@@ -0,0 +1,15 @@
+export function shuffleCards<T>(cards: T[]): T[] {
+  // Shuffle an array of cards
+  if (!Array.isArray(cards)) {
+    throw new TypeError('Input must be an array');
+  }
+  if (cards.length === 0) return [];
+
+  const shuffled = [...cards];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // Swap elements
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
