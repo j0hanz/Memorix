14c17eff06890d8dccbe80f8a6e51a9848cd6400
j0hanz <golfarenlj@gmail.com>
2025-03-19 16:35:54 +0100

    Add rollup-plugin-visualizer and vite-imagetools to enhance build analysis and image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
   plugins: [
@@ -10,12 +12,33 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+    imagetools(),
   ],
   resolve: {
     alias: {
       '@': '/src',
       '@components': '/src/components',
       '@utils': '/src/utils',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
