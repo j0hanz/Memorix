4a462427c70bd66035546f8cf59d952b898ae563
j0hanz <golfarenlj@gmail.com>
2025-03-19 11:04:49 +0100

    Remove unnecessary fragment

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -5,18 +5,16 @@ import { CategoryDataProps } from '@/types/components';
 // Pure data component for category selection
 export default function CategoryData({ onSelectCategory }: CategoryDataProps) {
   return (
-    <>
-      <div className={styles.btnCategoryContainer}>
-        {Object.entries(CATEGORY_INFO).map(([category, info]) => (
-          <div
-            key={category}
-            className={styles.btnCategorySelect}
-            onClick={() => onSelectCategory(category)}
-          >
-            {info.name}
-          </div>
-        ))}
-      </div>
-    </>
+    <div className={styles.btnCategoryContainer}>
+      {Object.entries(CATEGORY_INFO).map(([category, info]) => (
+        <div
+          key={category}
+          className={styles.btnCategorySelect}
+          onClick={() => onSelectCategory(category)}
+        >
+          {info.name}
+        </div>
+      ))}
+    </div>
   );
 }
