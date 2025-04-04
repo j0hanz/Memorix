e1920e7cb2b9a63b78bccd828349480ff5ee9199
j0hanz <golfarenlj@gmail.com>
2025-04-04 10:28:15 +0200

    Fix axios interceptor to ensure headers object is initialized before adding Authorization token

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -13,6 +13,7 @@ export const axiosReq = axios.create({
 axiosReq.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
+    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
