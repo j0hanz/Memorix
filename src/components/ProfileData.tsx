e1d572f6b667e73a6fb9d096e4c6dcc94e70790c
j0hanz <golfarenlj@gmail.com>
2025-04-05 14:18:54 +0200

    Refactor ProfileData component by updating image error handling and enhancing layout; add styles for image upload button and overlay for improved user experience.

--- src/components/ProfileData.tsx
+++ src/components/ProfileData.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Alert } from 'react-bootstrap';
 import Button from '@/components/Button';
 import styles from './styles/Modal.module.css';
@@ -9,6 +9,15 @@ import ImageIcon from '@mui/icons-material/Image';
 import TabNavigation from './TabNavigation';
 import { ProfileImageTabProps, ProfileSettingsTabProps } from '@/types/api';
 import { TabItem } from '@/types/components';
+import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
+
+// Utility function for handling image error
+const handleImageError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+  const target = e.target as HTMLImageElement;
+  target.onerror = null;
+  target.src =
+    'https://res.cloudinary.com/dxly7tpdi/image/upload/nobody_nrbk5n';
+};
 
 // Profile Image Tab Component
 const ProfileImageTab: React.FC<ProfileImageTabProps> = ({
@@ -22,11 +31,11 @@ const ProfileImageTab: React.FC<ProfileImageTabProps> = ({
   onClose,
 }) => {
   return (
-    <>
-      <Form onSubmit={handleUpdateProfile}>
-        <div className="d-flex flex-column">
-          <div className="row">
-            <div className="col d-flex justify-content-start">
+    <Form onSubmit={handleUpdateProfile}>
+      <div className="d-flex flex-column">
+        <div className="row">
+          <div className="col d-flex justify-content-start">
+            <div className="position-relative">
               <Image
                 src={
                   previewImage ||
@@ -35,49 +44,55 @@ const ProfileImageTab: React.FC<ProfileImageTabProps> = ({
                 }
                 alt="Profile"
                 className={styles.profileImage}
-                onError={(e) => {
-                  if (!e) return;
-                  const target = e.target as HTMLImageElement;
-                  target.onerror = null;
-                  target.src =
-                    'https://res.cloudinary.com/dxly7tpdi/image/upload/nobody_nrbk5n';
-                }}
+                onError={handleImageError}
               />
+              <div className={styles.usernameOverlay}>{user.username}</div>
             </div>
-            <div className="col d-flex flex-column justify-content-end">
-              {user.username}
-              <Form.Group className="my-3">
-                <Form.Label className="d-none">Profile Picture</Form.Label>
-                <Form.Control
+          </div>
+          <div className="col d-flex flex-column justify-content-end">
+            <Form.Group>
+              <Form.Label className="d-none">Profile Picture</Form.Label>
+              <div className="d-flex align-items-center">
+                <input
                   type="file"
+                  id="profilePictureInput"
                   accept="image/*"
                   onChange={handleImageChange}
+                  className={styles.fileInputHidden}
                 />
-
-                <Form.Text className="text-muted">
-                  Select a new profile picture to update
-                </Form.Text>
-              </Form.Group>
-            </div>
-          </div>
-          <div className="d-flex">
-            <Button
-              className={`${styles.btnRestart} ${styles.modalButton}`}
-              type="submit"
-              disabled={loading || !profileImage}
-            >
-              {loading ? 'Updating...' : 'Update Profile'}
-            </Button>
-            <Button
-              className={`${styles.btnExit} ${styles.modalButton}`}
-              onClick={onClose}
-            >
-              Close
-            </Button>
+                <div className={styles.btnImage}>
+                  <label
+                    htmlFor="profilePictureInput"
+                    className={styles.clickableLabel}
+                  >
+                    <DriveFolderUploadIcon className={styles.btnImageIcon} />
+                  </label>
+                </div>
+                <span>Upload image</span>
+              </div>
+              <Form.Text className="text-muted">
+                Select a new profile picture to update
+              </Form.Text>
+            </Form.Group>
           </div>
         </div>
-      </Form>
-    </>
+        <div className="d-flex">
+          <Button
+            className={`${styles.btnRestart} ${styles.modalButton}`}
+            type="submit"
+            disabled={loading || !profileImage}
+          >
+            {loading ? 'Updating...' : 'Update Profile'}
+          </Button>
+          <Button
+            className={`${styles.btnExit} ${styles.modalButton}`}
+            onClick={onClose}
+          >
+            Close
+          </Button>
+        </div>
+      </div>
+    </Form>
   );
 };
 
@@ -91,7 +106,6 @@ const ProfileSettingsTab: React.FC<ProfileSettingsTabProps> = ({
       <h5>Account Settings</h5>
       <p>Username: {user.username}</p>
       <p>Account created: {user.id ? `User ID: ${user.id}` : 'N/A'}</p>
-
       <div className="d-flex mt-4">
         <Button
           className={`${styles.btnExit} ${styles.modalButton}`}
@@ -154,13 +168,13 @@ const ProfileData: React.FC<{ onClose: () => void }> = ({ onClose }) => {
           user={user}
           profile={profile}
           loading={loading}
-          error={error}
-          success={success}
           profileImage={profileImage}
           previewImage={previewImage}
           handleImageChange={handleImageChange}
           handleUpdateProfile={handleUpdateProfile}
           onClose={onClose}
+          error={error}
+          success={success}
         />
       )}
       {activeKey === 'settings' && (
