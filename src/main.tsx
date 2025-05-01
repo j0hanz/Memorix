812667f9fe541cfe08e63a4f5e867abfd0b6610b
j0hanz <golfarenlj@gmail.com>
2025-05-01 09:06:38 +0200

    feat: add variables.css for Bootstrap overrides and color/style variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './variables.css';
 import '@fontsource/fredoka/400.css';
 import '@fontsource/fredoka/500.css';
 import '@/index.css';
