6c6d908cedc59e4f8a6032fd95abebe6f7693508
j0hanz <golfarenlj@gmail.com>
2025-06-07 14:28:04 +0200

    feat(useMotions): add toast and tooltip animations to motion hook

--- src/hooks/game/useMotions.ts
+++ src/hooks/game/useMotions.ts
@@ -5,6 +5,8 @@ import {
   FEEDBACK_VARIANTS,
   FLIP_VARIANTS,
   getStaggerConfig,
+  TOAST_VARIANTS,
+  TOOLTIP_VARIANTS,
 } from '@/constants/motions';
 
 export function useMotions() {
@@ -14,6 +16,8 @@ export function useMotions() {
     cardEntranceAnimation: CARD_ENTRANCE_VARIANTS,
     flipAnimation: FLIP_VARIANTS,
     cardContentAnimation: CARD_CONTENT_VARIANTS,
+    toastAnimation: TOAST_VARIANTS,
+    tooltipAnimation: TOOLTIP_VARIANTS,
     getStaggerConfig,
   };
 }
